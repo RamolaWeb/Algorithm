d363bcd859b9ce55cc02cca8bf26e933f1771584
RamolaWeb <sramola5@gmail.com>
2016-10-03 16:56:33 +0530

    Modified RESCALC

--- Question/RESCALC.cpp
+++ Question/RESCALC.cpp
@@ -7,46 +7,49 @@ int main(){
 		scanf("%d",&n);
 		int score[n];
 		for(i=0;i<n;i++){
-			int noc,j=0,sum=0;
+			int noc,j=0;
 			scanf("%d",&noc);
+			score[i]=noc;
 			int type[]={0,0,0,0,0,0};
 			for(j=0;j<noc;j++){
 				int ctype;
 				scanf("%d",&ctype);
 				type[--ctype]+=1;
 			}
-			for(j=0;j<=5;j++){
-				if(type[j]>=1)
-				sum++;
+			int zeros=0;
+			while(zeros<=2){
+					zeros=0;
+				for(j=0;j<=5;j++){
+				if(type[j]==0)
+                  zeros++;
+				  else
+				  type[j]-=1;					
+				}
+				if(zeros==0)
+				score[i]+=4;
+				else if(zeros==1)
+				score[i]+=2;
+				else if(zeros==2)
+				score[i]+=1;
 			}
-			switch(sum){
-				case 4:
-					score[i]=1+noc;
-					break;
-				case 5:
-					score[i]=2+noc;
-					break;
-				case 6:
-					score[i]=4+noc;
-					break;
-				default:
-				      score[i]=noc;
-					  break;			
-			}
-		}
-		int bigno=score[0],bigindex=0;
+	}
+		int bigno=score[0],bigindex=0,tiestatus=0;
 		for(i=1;i<n;i++)
 		{
 			if(bigno<score[i]){
 				bigno=score[i];
 				bigindex=i;
+				tiestatus=0;
 			}
 			else if(bigno==score[i]){
-				if(i==n-1){
-					bigindex=-1;
+				tiestatus=1;		
+				}
+					if(i==n-1){
+					if(tiestatus){
+						bigindex=-1;
 					printf("%s","tie");
 					break;
-				}
+					}
 			}
 			 
 		}
